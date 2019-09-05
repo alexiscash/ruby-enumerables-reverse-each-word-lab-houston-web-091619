82966c18750a237f38b12f8f0067e9b52a06d830
Alexis Rangel <alexiscash97@gmail.com>
2019-09-05 00:38:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   array = string.split
   new_array = []
-  array.each {|word| new_array << word.reverse }
+  array.collect {|word| new_array << word.reverse }
   new_array.join(' ')
   
 end
